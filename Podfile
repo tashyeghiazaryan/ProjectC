759b4e6ff4218357bc95f56a19f0f9a134cd8b89
Artashes Yeghiazaryan <Artashes.Egiazaryan@x5.ru>
2025-02-02 19:05:43 +0300

    first commit

---
+++ Podfile
@@ -0,0 +1,9 @@
+
+platform :ios, '13.0'
+use_frameworks!
+
+target 'ProjectC' do
+  pod 'GoogleMLKit/TextRecognition'
+  pod 'OpenCV'
+end
+
---
+++ ProjectC/CLogger.m
@@ -0,0 +1,16 @@
+//
+//  CLogger.m
+//  
+//
+//  Created by Artashes.Egiazaryan on 02.02.25.
+//
+
+#import "CLogger.h"
+
+@implementation CLogger
+
+-(void) functionC {
+    NSLog(@"Project C main action");
+}
+
+@end
